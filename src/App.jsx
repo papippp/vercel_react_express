d0ff2213f897daef511fb7abad08cc283705b59b
papippp <dlukzy@gmail.com>
2025-03-06 15:39:50 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+export default function App() {
+  const [message, setMessage] = useState('')
+  useEffect(() => {
+    fetch('https://vercel-express-api-rose.vercel.app/')
+      .then((response) => response.text())
+      .then((data) => setMessage(data))
+      .catch((error) => console.error('error fetching message', error))
+  }, [])
+  return (
+    <>
+      <p clasName='read-the-docs'>
+        {message ? message : 'loading message...'}
+
+      </p>
+
+    </>
+  )
+}
